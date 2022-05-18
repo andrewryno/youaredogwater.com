2305daeec08224ec058d09cfd02d0760abfb649f
Andrew Ryno <andrewryno@gmail.com>
2022-05-18 13:37:41 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          You are dog water. Iggy sends his regards.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
